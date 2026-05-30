3542e27543475660201475d509b5e7cf07065707
Serge Ivo <sivochkin@gmail.com>
2026-05-30 18:14:13 +1000

    fix: remove vitest/config reference

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,4 +1,3 @@
-/// <reference types="vitest/config" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -6,3 +5,4 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineConfig({
   plugins: [react(), tailwindcss()],
 });
+
